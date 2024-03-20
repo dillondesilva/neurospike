741f529a44b183d6fadd505054e5941fd74a79fc
dillondesilva <dillon.desilva@geeksdiary.com>
2024-03-20 14:17:13 +1100

    updates

--- neurospikelib/neurospikelib/testing_pulses.py
+++ neurospikelib/neurospikelib/testing_pulses.py
@@ -1,8 +1,7 @@
 from adex import AdEx
 
 AdEx.simulate(pulses=[{
-    "start": 10,
-    "end": 200,
-    "amp": 10
-}], membrane_c=1, resolution=100, sharpness=100, tau_w=1000,
-simulation_duration=202)
+  "start": 100,
+  "end": 400,
+  "amp": 10
+}], membrane_c=1, resolution=100, sharpness=100, tau_w=100, simulation_duration=500)
