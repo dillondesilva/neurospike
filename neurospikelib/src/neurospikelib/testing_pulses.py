0991c0995b726c7a08ecd327038e8e648d9ab719
dillondesilva <dillon.desilva@geeksdiary.com>
2024-05-27 22:32:22 +1000

    Updates

--- neurospikelib/src/neurospikelib/testing_pulses.py
+++
@@ -1,7 +0,0 @@
-from adex import AdEx
-
-AdEx.simulate(pulses=[{
-  "start": 100,
-  "end": 400,
-  "amp": 10
-}], membrane_c=1, resolution=100, sharpness=100, tau_w=100, simulation_duration=500)
---
+++ static/js/464.3970da7e.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkeduspike_web=self.webpackChunkeduspike_web||[]).push([[464],{8464:()=>{}}]);
