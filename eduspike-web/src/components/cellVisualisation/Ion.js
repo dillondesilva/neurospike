007504d667dc28366ce5c322c070cb7bc9929992
dillondesilva <dillon.desilva@geeksdiary.com>
2024-06-06 14:42:37 +1000

    Abstract class for Ion created. Subclasses to be implemented

---
+++ eduspike-web/src/components/cellVisualisation/Ion.js
@@ -0,0 +1,26 @@
+class Ion {
+    constructor(initialX, initialY, ionSize, ionColor) {
+        if (this.constructor === Ion) {
+            throw new Error("Attempting to initialise abstract class: Ion");
+        }
+
+        this.x = initialX;
+        this.y = initialY;
+        this.ionSize = ionSize;
+        this.ionColor = ionColor;
+    }
+
+    moveTo(locationX, locationY) {
+        // TODO: Implement this function to move
+        // ion in L shape towards the corresponding channel      
+    }
+
+    drawIon(p5Context) {
+        p5Context.push();
+        p5Context.fill(this.ionColor);
+        p5Context.circle(this.x, this.y, this.ionSize);
+        p5Context.pop();
+    }
+};
+
+export default Ion;
