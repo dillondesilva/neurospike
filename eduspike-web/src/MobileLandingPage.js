2b5e1080b93db1d430a66189ba8f279a646eca93
dillondesilva <dillon.desilva@geeksdiary.com>
2025-03-24 00:36:32 +1100

    Updated landing page title

--- eduspike-web/src/MobileLandingPage.js
+++ eduspike-web/src/MobileLandingPage.js
@@ -3,7 +3,7 @@ function MobileLandingPage() {
     <div>
         <div className="flex flex-col justify-center w-screen h-screen">
             {/* <img src='logo-white-bg.png' className="place-items-center  w-[35vw] h-[35vw]" alt="logo"/> */}
-            <h1 className="text-5xl self-center text-center" >Welcome to <h1 className="font-bold">Eduspike</h1></h1>
+            <h1 className="text-5xl self-center text-center" >Welcome to <h1 className="font-bold">Neurospike</h1></h1>
             <img src='logo-white-bg.png' className="mt-6 animate-pulse self-center w-[35vw] h-[35vw]" alt="logo"/>
             <p className="mt-6 text-center w-9/12 self-center">Understanding computational neurons has never been more intuitive.</p>
             <p className="mt-6 text-center w-full self-center">Try it out now<p className="font-bold">on desktop! Please use fullscreen for the optimal experience</p></p>
